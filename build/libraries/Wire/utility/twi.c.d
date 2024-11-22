70021c23063e372565b2c1c4515a064b59e9e582
Andrés Ayala <andresmayala@gmail.com>
2024-11-22 00:24:40 -0500

    chore: remove obsolete build files and caches

--- build/libraries/Wire/utility/twi.c.d
+++
@@ -1,6 +0,0 @@
-/Users/asalazargale/dev/arduino-smart-city-final/build/libraries/Wire/utility/twi.c.o: \
- /Users/asalazargale/Library/Arduino15/packages/arduino/hardware/avr/1.8.6/libraries/Wire/src/utility/twi.c \
- /Users/asalazargale/Library/Arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/Arduino.h \
- /Users/asalazargale/Library/Arduino15/packages/arduino/hardware/avr/1.8.6/cores/arduino/binary.h \
- /Users/asalazargale/Library/Arduino15/packages/arduino/hardware/avr/1.8.6/variants/mega/pins_arduino.h \
- /Users/asalazargale/Library/Arduino15/packages/arduino/hardware/avr/1.8.6/libraries/Wire/src/utility/twi.h
