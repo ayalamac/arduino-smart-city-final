6df27a7100007cf4f34ea329914135af27c480fb
Andrés Ayala <andresmayala@gmail.com>
2024-11-23 04:51:12 -0500

    Finish

--- smart-city/smart-city.ino
+++ smart-city/smart-city.ino
@@ -10,9 +10,9 @@
 
 // * I/O pin labeling
 
-#define LIGHT_INTENSITY_SENSOR A0 // Potenciómetro que simula la intensidad de la luz en el ambiente para regular la iluminación de los dos semáforos
-#define LDR2 A1 // Pendiente cómo usarlo...
-#define CO2 A2  // Sensor de CO2
+#define LIGHT_INTENSITY_SENSOR A0      // Potenciómetro que simula la intensidad de la luz en el ambiente para regular la iluminación de los dos semáforos
+#define AMBULANCE_PROXIMITY_SENSOR A1  // Potenciómetro que simula la proximidad de una ambulancia para darle prioridad en el paso
+#define CO2_SENSOR A2                         // Potenciómetro que simula la concentración de CO2 en el ambiente
 
 #define P1 37   // Botón que alterna entre modalidad de ciclo normal de semáforos y modalidad madrugada
 
@@ -59,20 +59,21 @@
 // -----------------------------------------------------------------------------------------------------------------
 
 // Máquina de estados
-#define ROJO_ROJO 0
-#define ROJO_NARANJA 1
-#define ROJO_VERDE 2
-#define ROJO_VERDE_T 3
-#define ROJO_AMARILLO 4
+#define ROJO_ROJO 100
+#define ROJO_NARANJA 200
+#define ROJO_VERDE 300
+#define ROJO_VERDE_T 400
+#define ROJO_AMARILLO 500
 
-#define ROJO_ROJO_V2 5
-#define NARANJA_ROJO 6
-#define VERDE_ROJO 7
-#define VERDE_T_ROJO 8
-#define AMARILLO_ROJO 9
+#define ROJO_ROJO_V2 600
+#define NARANJA_ROJO 700
+#define VERDE_ROJO 800
+#define VERDE_T_ROJO 900
+#define AMARILLO_ROJO 1000
 
 // Estado nocturno
-#define AMARILLO_T_ROJO_T 10
+#define AMARILLO_T_ROJO_T 1100
+#define PASO_AMBULANCIA_VIA1 1200
 
 // -----------------------------------------------------------------------------------------------------------------
 
@@ -92,7 +93,7 @@ const float CO2Curve[3] = {2.602, ZERO_POINT_VOLTAGE, (REACTION_VOLTAGE / (2.602
 // Tiempos para cada estado (en milisegundos)
 const unsigned long DURACION_ROJO       = 1000;
 const unsigned long DURACION_NARANJA    = 2000;
-const unsigned long DURACION_VERDE      = 3000;
+unsigned long duracionVerde      = 3000;
 const unsigned long DURACION_VERDE_T    = 2000;
 const unsigned long DURACION_AMARILLO   = 1000;
 const unsigned long DURACION_TITILACION = 250;
@@ -105,10 +106,11 @@ float co2 = 0;   // Variable to store CO2 value
 bool vP1       = false;
 bool vP1Previo = false;
 bool vP2       = false;
+bool vP2Previo = false;
 
 int ambientLightLevel = 255;
 int currentBrightness = 1;
-int vLDR2 = 0;
+int ambulanceRelativePosition = 0;
 int vCO2 = 0;
 float dCO2 = 0;
 
@@ -121,42 +123,39 @@ void actualizarSemaforo(int pinRed, int pinYellow, int pinGreen, int redLevel, i
     analogWrite(pinGreen, greenLevel * currentBrightness);
 }
 
-void mostrarEstadoLCD() {
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Estado:");
-    switch (state) {
-        case ROJO_ROJO:
-            lcd.print(" ROJO");
-            break;
-        case ROJO_AMARILLO:
-        case AMARILLO_ROJO:
-            lcd.print(" AMARILLO");
-            break;
-        case ROJO_VERDE:
-            lcd.print(" VERDE");
-            break;
-    }
-    lcd.setCursor(0, 1);
-    lcd.print("Tiempo:");
-    lcd.print((previousMillis + ((state == ROJO_ROJO || state == ROJO_VERDE) ? DURACION_ROJO : DURACION_AMARILLO)) - millis());
-}
+// void mostrarEstadoLCD() {
+//     lcd.clear();
+//     lcd.setCursor(0, 0);
+//     lcd.print("Estado:");
+//     switch (state) {
+//         case ROJO_ROJO:
+//             lcd.print(" ROJO");
+//             break;
+//         case ROJO_AMARILLO:
+//         case AMARILLO_ROJO:
+//             lcd.print(" AMARILLO");
+//             break;
+//         case ROJO_VERDE:
+//             lcd.print(" VERDE");
+//             break;
+//     }
+//     lcd.setCursor(0, 1);
+//     lcd.print("Tiempo:");
+//     lcd.print((previousMillis + ((state == ROJO_ROJO || state == ROJO_VERDE) ? DURACION_ROJO : DURACION_AMARILLO)) - millis());
+// }
 
 bool estaEnModalidadMadrugada = false;
+bool botonPeatonalsolicitado = false;
 
 void readAllData()
 {
     ambientLightLevel = analogRead(LIGHT_INTENSITY_SENSOR);
+    ambulanceRelativePosition = analogRead(AMBULANCE_PROXIMITY_SENSOR);
 
-    currentBrightness = map(ambientLightLevel, 0, 1023, 32, 255);
-    currentBrightness = ((currentBrightness - 32) / 32) * 32 + 32;
-    currentBrightness = constrain(currentBrightness, 32, 255);
-
-    vLDR2 = analogRead(LDR2);
-    vCO2 = analogRead(CO2);
+    vCO2 = analogRead(CO2_SENSOR);
 
     dCO2 = 0;
-    volts = analogRead(CO2) * 5.0 / 1023.0;
+    volts = analogRead(CO2_SENSOR) * 5.0 / 1023.0;
     if (volts / DC_GAIN >= ZERO_POINT_VOLTAGE)
     {
         dCO2 = -1;
@@ -173,6 +172,17 @@ void readAllData()
         estaEnModalidadMadrugada = !estaEnModalidadMadrugada;
     }
     vP1Previo = vP1;
+
+    botonPeatonalsolicitado = false;
+
+    if (vP2 && !vP2Previo) {
+        // Reducir tiempo de verde en semáforo 2
+        if (state == ROJO_VERDE && !botonPeatonalsolicitado ) {
+            duracionVerde = duracionVerde - 2000;
+            botonPeatonalsolicitado = true;
+        }
+    }
+    vP2Previo = vP2;
 
 }
 
@@ -213,9 +223,63 @@ void setup()
     previous_state = ROJO_AMARILLO;
 }
 
+void calculateBrightnessLevel() {
+    currentBrightness = map(ambientLightLevel, 0, 1023, 32, 255);
+    currentBrightness = ((currentBrightness - 32) / 32) * 32 + 32;
+    currentBrightness = constrain(currentBrightness, 32, 255);
+}
 
+bool isAmbulancePassingThrough() {
+    int proximityRate = (trafficLevelManager1.getAdditionalTime() + 1000) / 1000;
+    int requiredProximity = 128 * proximityRate;
+    int startThreshold = 511 - requiredProximity;
+    int endThreshold = 511 + requiredProximity;
+    return ambulanceRelativePosition >= startThreshold && ambulanceRelativePosition <= endThreshold;
+}
+
+long lastime = 0;
+String modoUso = "Ciclo Normal   ";
 void loop() {
     readAllData();
+
+    if (millis() - lastime > 1000) {
+
+        Serial.println(String(vCO2)); // Para visualizar en el monitor con Ubidots
+
+        if (botonPeatonalsolicitado) { modoUso = "Paso Peatonal solicitado"; }
+        if (estaEnModalidadMadrugada) { modoUso = "Madrugada      "; }
+        if (isAmbulancePassingThrough()) { modoUso = "Paso Ambulancia"; }
+        // normal de nuevo
+        if (!botonPeatonalsolicitado && !estaEnModalidadMadrugada && !isAmbulancePassingThrough()) { modoUso = "Ciclo Normal   "; }
+
+        lastime = millis();
+
+        lcd.setCursor(0, 0);
+        lcd.print("---- SMART CITY ----");
+
+        lcd.setCursor(0, 1);
+        lcd.print("Modo de Semaforo:");
+
+        lcd.setCursor(0, 2);
+        lcd.print(modoUso);
+        
+        // lcd.setCursor(5, 0); lcd.print(vLDR1);
+        // lcd.setCursor(5, 1); lcd.print(vLDR2);
+        // lcd.setCursor(5, 2); lcd.print(vCO2);
+        // lcd.setCursor(3, 3); lcd.print(1 * vP1);
+        // lcd.setCursor(8, 3); lcd.print(1 * vP2);
+        // lcd.setCursor(15, 1); lcd.print(1 * vCNY1);
+        // lcd.setCursor(17, 1); lcd.print(1 * vCNY2);
+        // lcd.setCursor(19, 1); lcd.print(1 * vCNY3);
+        // lcd.setCursor(15, 3); lcd.print(1 * vCNY4);
+        // lcd.setCursor(17, 3); lcd.print(1 * vCNY5);
+        // lcd.setCursor(19, 3); lcd.print(1 * vCNY6);
+
+        // mostrarEstadoLCD();
+    }
+
+    calculateBrightnessLevel();
+
     trafficLevelManager1.updateTrafficLevels();
     trafficLevelManager2.updateTrafficLevels();
 
@@ -246,7 +310,7 @@ void loop() {
 
             if (currentMillis - previousMillis >= DURACION_NARANJA) {
                 previousMillis = currentMillis;
-                state = VERDE_ROJO;
+                state = !isAmbulancePassingThrough() ? VERDE_ROJO : PASO_AMBULANCIA_VIA1;
             }
             trafficLevelManager1.calculateAdditionalTime();
             trafficLevelManager2.calculateAdditionalTime();
@@ -256,12 +320,12 @@ void loop() {
             actualizarSemaforo(S1LR, S1LA, S1LV, (int)false, (int)false, (int)true);
             actualizarSemaforo(S2LR, S2LA, S2LV, (int)true, (int)false, (int)false);
 
-            sevenSegmentManager1.print(((trafficLevelManager1.getAdditionalTime() + DURACION_VERDE + DURACION_VERDE_T + DURACION_AMARILLO + 1000) - (currentMillis - previousMillis)) / 1000);
+            sevenSegmentManager1.print(((trafficLevelManager1.getAdditionalTime() + duracionVerde + DURACION_VERDE_T + DURACION_AMARILLO + 1000) - (currentMillis - previousMillis)) / 1000);
             sevenSegmentManager2.print(-1);
 
-            if (currentMillis - previousMillis >= trafficLevelManager1.getAdditionalTime() + DURACION_VERDE) {
+            if (currentMillis - previousMillis >= trafficLevelManager1.getAdditionalTime() + duracionVerde) {
                 previousMillis = currentMillis;
-                state = VERDE_T_ROJO;
+                state = !isAmbulancePassingThrough() ? VERDE_T_ROJO : PASO_AMBULANCIA_VIA1;
                 trafficLevelManager1.reset();
             }
             trafficLevelManager2.calculateAdditionalTime();
@@ -275,7 +339,8 @@ void loop() {
 
             if (currentMillis - previousMillis >= DURACION_VERDE_T) {
                 previousMillis = currentMillis;
-                state = AMARILLO_ROJO;
+                state = !isAmbulancePassingThrough() ? AMARILLO_ROJO : PASO_AMBULANCIA_VIA1;
+
             }
             trafficLevelManager2.calculateAdditionalTime();
             break;
@@ -289,7 +354,8 @@ void loop() {
             if (currentMillis - previousMillis >= DURACION_AMARILLO) {
                 sevenSegmentManager1.print(-1);
                 previousMillis = currentMillis;
-                state = ROJO_ROJO_V2;
+                state = !isAmbulancePassingThrough() ? ROJO_ROJO_V2 : PASO_AMBULANCIA_VIA1;
+
             }
             trafficLevelManager1.calculateAdditionalTime();
             trafficLevelManager2.calculateAdditionalTime();
@@ -328,12 +394,14 @@ void loop() {
             actualizarSemaforo(S2LR, S2LA, S2LV, (int)false, (int)false, (int)true); // Luz verde
             
             sevenSegmentManager1.print(-1);
-            sevenSegmentManager2.print(((trafficLevelManager2.getAdditionalTime() + DURACION_VERDE + DURACION_VERDE_T + DURACION_AMARILLO + 1000) - (currentMillis - previousMillis)) / 1000);
+            sevenSegmentManager2.print(((trafficLevelManager2.getAdditionalTime() + duracionVerde + DURACION_VERDE_T + DURACION_AMARILLO + 1000) - (currentMillis - previousMillis)) / 1000);
 
-            if (currentMillis - previousMillis >= trafficLevelManager2.getAdditionalTime() + DURACION_VERDE) {
+            if (currentMillis - previousMillis >= trafficLevelManager2.getAdditionalTime() + duracionVerde) {
                 previousMillis = currentMillis;
                 state = ROJO_VERDE_T;
                 trafficLevelManager2.reset();
+                duracionVerde = 3000;
+                botonPeatonalsolicitado = false;
             }
             trafficLevelManager1.calculateAdditionalTime();
             break;
@@ -361,7 +429,7 @@ void loop() {
 
             if (currentMillis - previousMillis >= DURACION_AMARILLO) {
                 previousMillis = currentMillis;
-                state = ROJO_ROJO;
+                state = !isAmbulancePassingThrough() ? ROJO_ROJO : PASO_AMBULANCIA_VIA1;
             }
             trafficLevelManager1.calculateAdditionalTime();
             trafficLevelManager2.calculateAdditionalTime();
@@ -370,13 +438,14 @@ void loop() {
         case AMARILLO_T_ROJO_T:
             currentBrightness = 255;
             actualizarSemaforo(S1LR, S1LA, S1LV, (int)false, ((currentMillis - previousMillis) / (2 * DURACION_TITILACION)) % 2, (int)false); // Luz amarilla titilando
-            actualizarSemaforo(S2LR, S2LA, S2LV, ((currentMillis -(int) previousMillis) / (2 * DURACION_TITILACION)) % 2, (int)false, (int)false); // Luz roja titilando
+            actualizarSemaforo(S2LR, S2LA, S2LV, ((currentMillis - previousMillis) / (2 * DURACION_TITILACION)) % 2, (int)false, (int)false); // Luz roja titilando
             state = estaEnModalidadMadrugada ? AMARILLO_T_ROJO_T : ROJO_ROJO;
             break;
+
+        case PASO_AMBULANCIA_VIA1:
+            actualizarSemaforo(S1LR, S1LA, S1LV, (int)false, ((currentMillis - previousMillis) / (2 * DURACION_TITILACION)) % 2, (int)true); // Luz amarilla titilando
+            actualizarSemaforo(S2LR, S2LA, S2LV, (int)true, ((currentMillis - previousMillis) / DURACION_TITILACION) % 2, (int)false); // Luz roja titilando
+            state = isAmbulancePassingThrough() ? PASO_AMBULANCIA_VIA1 : ROJO_ROJO_V2;
+            break;
     }
-
-    // Actualizar la pantalla LCD con el estado actual
-    mostrarEstadoLCD();
 }
-
-
